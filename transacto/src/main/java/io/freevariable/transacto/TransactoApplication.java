4deec78fb5bfe2018d802c60cc92b2e31af8c3dd
a-fatah <abdulfatah66@gmail.com>
2023-03-22 23:21:52 +0100

    add transacto

---
+++ transacto/src/main/java/io/freevariable/transacto/TransactoApplication.java
@@ -0,0 +1,13 @@
+package io.freevariable.transacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactoApplication.class, args);
+    }
+
+}
---
+++ transacto/src/test/java/io/freevariable/transacto/TransactoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.freevariable.transacto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
